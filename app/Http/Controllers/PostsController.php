662ca59529c1a86b0a5c2c006551cd14e064bca8
Shaf3y <Shaf3iibrahim@gmail.com>
2019-03-14 09:59:46 +0200

    making authorize to true

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Post\StorePostRequest;
 
 class PostsController extends Controller
 {
