5e05122c93ec6da2e46219984618d8297b50ab2a
Shaf3y <Shaf3iibrahim@gmail.com>
2019-03-15 09:23:30 +0200

    adding route to api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/posts', 'PostsController@index');
+
+Route::get('ay-7aga', function () {
+    return "ajsdh<dsasd";
+});
